3390a50d1618d84b372355dd004a9c8a353e430f
Gabodp <39098519+Gabodp@users.noreply.github.com>
2019-12-04 15:06:56 -0500

    cambios en html

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "hotel",
         'USER': "postgres",
-        'PASSWORD': "admin",
+        'PASSWORD': "root",
         'HOST': "localhost",
         'PORT': "5432",
     }
